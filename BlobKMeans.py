fec38865e006e76404cd470b92cc7359aadcce34
Monica Chan <mchan2021@pingry.org>
2020-12-02 18:59:14 -0500

    Update 122

--- BlobKMeans.py
+++ BlobKMeans.py
@@ -1,4 +1,36 @@
 from sklearn.datasets import make_blobs
 import numpy as np 
+from KMeansImplementation import KMeansImplementation
 
 points, categories = make_blobs(n_samples = 150, n_features = 2, centers = 4, cluster_std = 0.5, shuffle = True, random_state = 0)
+points = np.array(points)
+
+kmeans = KMeansImplementation(k = 4, max_iter = 200, tol = 0.05)
+blob_assignment, centroid_coordinates = kmeans.kmean_implement(points)
+print(blob_assignment)
+print(centroid_coordinates)
+
+"""def initialize_centroids(points, k):
+    shuffled_points = points.copy()
+    np.random.shuffle(shuffled_points)
+    return shuffled_points[:k]
+
+centroid_list = np.array(initialize_centroids(points, 4))
+
+def find_closest_centroid(points, centroids):
+    #returns an array containing the index to the nearest centroid for each point
+    distances = np.linalg.norm(centroids[:, np.newaxis] - points, axis = 2)
+    return np.argmin(distances, axis=0)
+
+closest_centroids = find_closest_centroid(points, centroid_list)
+
+
+def find_new_centroids(points, centroids, assigned_centroids):
+        return_array = []
+        for i in range(centroids.shape[0]):
+            return_array.append(points[assigned_centroids==i].mean(axis = 0)) 
+        return np.array(return_array)
+    
+
+print(closest_centroids)
+print(find_new_centroids(points, centroid_list, closest_centroids))"""
--- KMeansImplementation.py
+++ KMeansImplementation.py
@@ -1,49 +1,101 @@
+"""
+TODO:
+- change distance to scipy minkowski distance
+- insert blob graphs 
+
+"""
+
 import numpy as np
-class K_Means:
-    def __init__(self, k, tol = 0.001, max_iter = 300):
+import matplotlib as plt
+
+class KMeansImplementation:
+    def __init__(self, k, max_iter, tol):
         self.k = k
         self.tol = tol
         self.max_iter = max_iter
-
-    def centroids(self, data):
-
-        #this section generates the centroid list
-        centroid_list = []
-        for i in range(self.k):
-            centroid_list[i] = np.random.choice(data)
-
-        #this section determines which centroid each data point is closest to
-        assigned_centroids = []
+    def kmean_implement(self, data):
+        done = False 
+        iteration = 0 
+        previous_sse = 0    
         
-        for i in data:
-            closest = centroid_list[0]
-            closest_distance = np.linalg.norm(centroid_list[0]-i)
-            for j in centroid_list:
-                dist = np.linalg.norm(j-i)
-                if dist < closest_distance:
-                    closest_distance = dist
-                    closest = j 
-            np.append(assigned_centroids, closest, axis = 0)
+        ten_centroids = np.array(10,1)
+        ten_sse = np.array(10,1)
+        #this for loop will generate the best of 10 initial clusters:
 
-        classification = np.append(data,assigned_centroids, axis = 1) 
+        for i in range(0,10):
+            centroid_list = initialize_centroids(data, self.k)
+
+            assigned_centroids = find_closest_centroid(data, centroid_list)
+            
+            #this section determines the new centroids through averages
+            new_centroids = find_new_centroids(data, centroid_list, assigned_centroids)
+                        
+            #this section reassigns the data to the new centroids 
+            new_assigned = find_closest_centroid(data, new_centroids)
+
+            #this section is for algorithm verification
+            sse = get_errors(data, new_assigned, new_centroids)
+            ten_centroids[i] = new_centroids
+            ten_sse[i] = sse
+
+        centroid_list = ten_centroids[np.argmin(ten_sse)]
+        while done == False:
+            #this section generates the centroid list
         
-        #this section determines the new centroids
-        new_centroids = []
-        for i in centroid_list:
-            temp = []
-            for j in classification:
-                if j[1] == i:
-                    np.append(temp, j[0])
-            np.append(new_centroids, np.average(temp))
+            #this section determines which centroid each data point is closest to
+            assigned_centroids = find_closest_centroid(data, centroid_list)
+            
+            #this section determines the new centroids through averages
+            new_centroids = find_new_centroids(data, centroid_list, assigned_centroids)
+                        
+            #this section reassigns the data to the new centroids 
+            new_assigned = find_closest_centroid(data, new_centroids)
+
+            #this section is for algorithm verification
+            
+            current_sse = get_errors(data, new_assigned, new_centroids)
+            if (iteration == self.max_iter) or (abs(previous_sse-current_sse) <= self.tol and iteration != 0):
+                done = True
+                print("Previous SSE: ", previous_sse)
+                print( "Current SSE: ", current_sse)
+                print("difference: ", abs(current_sse-previous_sse))
+                print(iteration)
+                return np.concatenate((data, new_assigned[:,np.newaxis]), axis = 1), new_centroids
+                
+            else:
+                print("Previous SSE: ", previous_sse)
+                print( "Current SSE: ", current_sse)
+                print("difference: ", abs(current_sse-previous_sse))
+                previous_sse = current_sse
+                centroid_list = new_centroids
+                iteration += 1
+                print(iteration)
+                done = False
+    
+def initialize_centroids(points, k):
+    shuffled_points = points.copy()
+    np.random.shuffle(shuffled_points)
+    return shuffled_points[:k]
         
-        #this section reassigns the data to the new centroids 
-        new_assigned = []
-        for i in data:
-            closest = new_centroids[0]
-            closest_distance = np.linalg.norm(new_centroids[0]-i)
-            for j in new_centroids:
-                dist = np.linalg.norm(j-i)
-                if dist < closest_distance:
-                    closest_distance = dist
-                    closest = j 
-            np.append(new_assigned, closest, axis = 0)        
+def find_closest_centroid(points, centroids):
+    """returns an array containing the index to the nearest centroid for each point"""
+    distances = np.linalg.norm(centroids[:, np.newaxis] - points, axis = 2)
+    return np.argmin(distances, axis=0)
+    
+def find_new_centroids(points, centroids, assigned_centroids):
+    return_array = []
+    for i in range(centroids.shape[0]):
+        return_array.append(points[assigned_centroids==i].mean(axis = 0)) 
+    return np.array(return_array)
+    
+def get_errors(points, centroids, centroid_coordinates):
+    centroids = centroids.astype(int)
+    errors = np.sum((points - centroid_coordinates[centroids])**2)
+    return errors  
+        
+    
+                
+    
+            
+            
+
